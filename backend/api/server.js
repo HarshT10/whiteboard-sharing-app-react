095315b4c7a9a030c4609a9acc7a571f524cfce5
Harsh Thakkar <harsh.thakkar1011@gmail.com>
2024-06-02 11:00:54 +0530

    trial

---
+++ backend/api/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const { addUser, removeUser, getUser } = require("../utils/users");
+
+const app = express();
+
+const server = require("http").createServer(app);
+const io = new Server(server);
+
+let meetingCodeGlobal, imageURLGlobal;
+
+io.on("connection", (socket) => {
+  socket.on("userJoined", (data) => {
+    const { name, meetingCode, userId, host, presenter } = data;
+
+    meetingCodeGlobal = meetingCode;
+    socket.join(meetingCode);
+    const users = addUser({
+      name,
+      meetingCode,
+      userId,
+      host,
+      presenter,
+      socketId: socket.id,
+    });
+
+    socket.emit("userIsJoined", { success: true, users });
+
+    socket.broadcast.to(meetingCode).emit("userJoinedMessage", name);
+
+    socket.broadcast.to(meetingCode).emit("allUsers", users);
+
+    socket.broadcast.to(meetingCode).emit("whiteBoardDataResponse", {
+      imageURL: imageURLGlobal,
+    });
+  });
+
+  socket.on("whiteBoardData", (data) => {
+    imageURLGlobal = data;
+    socket.broadcast.to(meetingCodeGlobal).emit("whiteBoardDataResponse", {
+      imageURL: imageURLGlobal,
+    });
+  });
+
+  socket.on("message", (data) => {
+    const { message } = data;
+    const user = getUser(socket.id);
+    if (user) {
+      socket.broadcast
+        .to(meetingCodeGlobal)
+        .emit("messageResponse", { message, name: user.name });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const user = getUser(socket.id);
+    if (user) {
+      removeUser(socket.id);
+      socket.broadcast.to(meetingCodeGlobal).emit("userLeftMessage", user.name);
+    }
+  });
+});
+
+module.exports = (req, res) => {
+  server.emit("request", req, res);
+};
